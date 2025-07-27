1e543abee9cd12b053c8653c7546a8bff632642a
AtsuyaNakamura803 <atuya.0803@gmail.com>
2025-07-27 12:31:45 +0900

    Application.java

---
+++ Application.java
@@ -0,0 +1,30 @@
+package raisetech.Student.Management;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@Autowired
+	private StudentRepository repository;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return repository.search();
+	}
+
+	@GetMapping("/studentsCourseList")
+	public List<StudentsCourses> getStudentsCourseList() {
+		return repository.searchStudentsCourses();
+	}
+}
